7b5f96fb46f9f4708276cf197eb3964b31db73c0
Nathan Provost <nprovost.pro@gmail.com>
2023-05-11 08:28:48 +0200

    'init_commit

---
+++ com/example/efrei_api_project/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.efrei_api_project;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
